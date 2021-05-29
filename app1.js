b2b6bbd9df74a34a99fb6e412daf44bd84990c75
christopherivy <cdi.747@gmail.com>
2021-05-29 11:10:33 -0700

    meme generator finished

--- app1.js
+++ app1.js
@@ -13,13 +13,46 @@ btn.addEventListener("click", function (e) {
 	let topTextInput = topText.value;
 	let bottomTextInput = bottomText.value;
 
-	let newDiv = document.createElement("div");
-	memeContainer.append(newDiv);
-	newDiv.classList.add("newdiv");
-	let memeImg = document.createElement("img");
-	memeImg.classList.add("image");
-	newDiv.append(memeImg);
-	memeImg.src = urlInput;
+	if (!urlInput) return;
 
-	console.log(newDiv);
+	//newdiv
+	let newDiv = document.createElement("div");
+
+	//add the newdiv to the meme container with a class
+	memeContainer.append(newDiv);
+	newDiv.classList.add("newDiv");
+
+	//create the top text div
+	let topTextDiv = document.createElement("div");
+	newDiv.append(topTextDiv);
+	topTextDiv.classList.add("top-text", "memeText");
+	topTextDiv.innerText = topTextInput;
+
+	//create the img text div
+	let imgDiv = document.createElement("div");
+	newDiv.append(imgDiv);
+	imgDiv.classList.add("imageDiv");
+	let memeImg = document.createElement("img");
+	memeImg.src = urlInput;
+	memeImg.classList.add("image");
+	imgDiv.append(memeImg);
+
+	//create the bottom text div
+	let bottomTextDiv = document.createElement("div");
+	newDiv.append(bottomTextDiv);
+	bottomTextDiv.classList.add("bottom-text", "memeText");
+	bottomTextDiv.innerText = bottomTextInput;
+
+	//clear out input fields
+	url.value = "";
+	topText.value = "";
+	bottomText.value = "";
+});
+
+memeContainer.addEventListener("click", function (e) {
+	e.preventDefault();
+	let deleteMeme = e.target.offsetParent.offsetParent;
+	if (deleteMeme !== null && deleteMeme.classList.contains("newDiv")) {
+		deleteMeme.remove();
+	}
 });
